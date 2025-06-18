d61e0acfdcc9304329a021260146c076aedbcea5
maximiliano1357 <maxitimoner2@gmail.com>
2025-06-18 16:53:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+// Ejemplo de código para manejar el envío del formulario (esto es solo un ejemplo básico)
+document.querySelector('form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Formulario enviado. ¡Gracias por contactarnos!');
+});
+
